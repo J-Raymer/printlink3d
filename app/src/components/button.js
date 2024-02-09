f12869dc6755e485389003d5718968ab2ccd67eb
James Vandersluis <jamesvandersluis@outlook.com>
2024-02-08 23:21:01 -0800

    add: multistep forms, sidebar, and basic button
    Adds the multistep form and the multistep pages
    Adds the skeleton of the form to both the browse and create page
    Added some more colour and styling to the application
    Everything is still rather bare bones and will need to be styled according to the spec
    Button is currently just an example of a simple component, however another pull should be done to give it proper styling

---
+++ app/src/components/button.js
@@ -0,0 +1,5 @@
+export default function Button({text}) {
+  return (
+    <button className="">{text}</button>
+  );
+}
--- app/src/routes/create.js
+++ app/src/routes/create.js
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import MultiStepForm from "../components/multistepform";
+import MultiStepFormPage from "../components/multistepformpage";
 
 export default function Create() {
   return (
     <div>
-      <h1 className="text-4xl font-extrabold">Create</h1>
+      <h1 className="text-4xl font-extrabold p-6 pl-4">Create</h1>
+      <MultiStepForm submitText="Submit Job">
+        <MultiStepFormPage title="Upload">
+          <h1>Step 1</h1>
+        </MultiStepFormPage>
+        <MultiStepFormPage title="Configure">
+          <h1>Step 2</h1>
+        </MultiStepFormPage>
+      </MultiStepForm>
       <Link to="/">Back</Link>
     </div>
   );
