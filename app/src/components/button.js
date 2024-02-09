956c471896d467948aa9e0ad719aa21b28bdae48
James Vandersluis <jamesvandersluis@outlook.com>
2024-02-09 12:54:40 -0800

    fix: changed div to empty, added default btn class
    Changed div to empty tag in multistepformpage
    Added default button styles for Button component. This should be updated to match exact spec.

--- app/src/components/button.js
+++ app/src/components/button.js
@@ -1,5 +1,5 @@
 export default function Button({text}) {
   return (
-    <button className="">{text}</button>
+    <button className="bg-blue-500 hover:bg-blue-700 border border-blue-500 text-white font-bold py-2 px-4 rounded">{text}</button>
   );
 }
--- app/src/components/multistepformpage.js
+++ app/src/components/multistepformpage.js
@@ -1,7 +1,7 @@
 export default function MultiStepFormPage({children, title}) {
   return (
-    <div>
+    <>
       {children}
-    </div>
+    </>
   );
 };
