f7e043c14f2dc2658e889af25864e5dcc3f62496
DanielVette <156377663+DanielVette@users.noreply.github.com>
2024-02-09 11:32:20 -0800

    Firebase
    Added firebase connection and base functions

--- app/src/app.js
+++ app/src/app.js
@@ -29,3 +29,57 @@ function App() {
 }
 
 export default App;
+
+async function AddJob(db) {
+  const docRef = await addDoc(collection(db, "Job"), {
+    Customer_ID: 1,
+    Fill_Percentage: 0.1,
+    ID: 1,
+    Material: "Plasic",
+    Printer_ID: 1,
+    Radius: 0.1,
+    STL: "filepath",
+    Status: false
+  });
+}
+
+async function AddCustomer(db){
+  const docRef = await addDoc(collection(db,"Customers"), {
+    Email: "customer_email@gmail.com",
+    ID: 1
+  });
+}
+
+async function AddPrinter(db){
+  const docRef = await addDoc(collection(db,"Printers"), {
+    Email: "printer_email@gmail.com",
+    ID: 1
+});
+}
+
+async function GetJob(field,comp,value,db){
+  const job = collection(db,"Job");
+  q = query(job,where(field,comp,value));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot;
+}
+
+async function GetCustomer(field,comp,value,db){
+  const job = collection(db,"Customers");
+  q =  query(job,where(field,comp,value));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot;
+}
+
+async function GetPrinters(field,comp,value,db){
+  const job = collection(db,"Printers");
+  q = query(job,where(field,comp,value));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot;
+}
+
+async function GetMaterial(db){
+  const docRef = collection(db,"Material");
+  const querySnapshot = await getDocs(docRef);
+  return querySnapshot;
+}
