ff30a7954e632253480985632c9b71eed315b065
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-11 16:28:30 -0800

    Style home page according to mockups

--- app/src/routes/home.js
+++ app/src/routes/home.js
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import cube_icon from "../images/cube_icon.png";
+import printing_icon from "../images/printing_icon.png";
+import Button from "../components/button.js"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-extrabold p-6 pl-0">Welcome!</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <Link to="/create" className="cta-button"><span className="text-3xl">I need something printed</span></Link>
-        <Link to="/browse" className="cta-button"><span className="text-3xl">I can print</span></Link>
+    // <div className="flex items-center justify-center h-screen">
+    //   <div className="flex items-center">
+    //     <div className="flex flex-col items-center">
+    //       <img src={cube_icon} alt="I need printing icon" className="mb-2"/>
+    //       <p className="text-center text-8xl font-bold">I need printing</p>
+    //     </div>
+    //     <div className="w-px h-24 bg-blue-300 mx-8"></div>
+    //     <div className="flex flex-col items-center">
+    //       <img src={printing_icon} alt="I can print icon" className="mb-2"/>
+    //       <p className="text-center text-8xl font-bold">I can print</p>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="flex justify-between h-screen">
+      <div className="w-1/2 border-r-2 flex flex-col items-center justify-start">
+        <img src={cube_icon} alt="I need printing icon" className="mb-2"/>
+        <h1 className="text-8xl">I need printing</h1>
+        <Link className="mt-10" to="/create">
+          <Button text="List Job"/>
+        </Link>
+      </div>
+      <div className="w-1/2 flex flex-col items-center justify-start">
+        <img src={printing_icon} alt="I can print icon" className="mb-2"/>
+        <h1 className="text-8xl">I can print</h1>
+        <Link className="mt-10" to="/browse">
+          <Button text="Browse Jobs"/>
+        </Link>
       </div>
     </div>
   );
