ad4fd908737f4755582e173caf9cebf5d20119b5
DH3773 <dylanhugheswork@gmail.com>
2024-02-14 20:45:25 -0800

    Banner Gradient Complete

---
+++ app/src/components/home_button.js
@@ -0,0 +1,7 @@
+export default function Button({text}) {
+    return (
+        <button className="bg-transparent hover:text-red-700 text-white font-bold py-2 px-4 rounded">{text}</button>
+    );
+}
+
+  
