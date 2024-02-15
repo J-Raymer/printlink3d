c08813b983004a92d6ff347c7bd9e79cad1d3889
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-15 14:50:56 -0800

    Fix multiple jobs being selected at once
    This commit disallows multiple jobs to be selected during the job selection phase.
    
    Also lowers the amount of time for the border to appear

--- app/src/components/JobCard.jsx
+++ app/src/components/JobCard.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from 'react';
 import "./JobCard.css";
 
-export default function JobCard({ title, material, infill, image }) {
-  const [isSelected, setIsSelected] = useState(false);
+export default function JobCard({ job, isSelected, onSelectJob }) {
 
   const handleClick = () => {
-    setIsSelected(!isSelected);
+    onSelectJob(job);
   };
 
   return (
     <div
       onClick={handleClick}
-      className={`max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden md:flex w-full transition-all duration-300 transform hover:scale-105 ${
+      className={`max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden md:flex w-full transition-all duration-50 transform hover:scale-105 ${
         isSelected ? 'border-8 border-gradient' : ''
       }`}
       style={{ height: isSelected ? 'auto' : 'fit-content' }}
     >
       <div className="md:flex-shrink-0">
-        <img className="h-48 w-full object-cover md:w-48" src={image} alt={title} />
+        <img className="h-48 w-full object-cover md:w-48" src={job.image} alt={job.title} />
       </div>
       <div className="p-8">
-        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title}</div>
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{job.title}</div>
         <p className="mt-2 text-gray-600">
-          <span className="font-semibold">Material: </span><span className="font-normal">{material}</span>
+          <span className="font-semibold">Material: </span><span className="font-normal">{job.material}</span>
         </p>
         <p className="mt-2 text-gray-600">
-          <span className="font-semibold">Infill: </span><span className="font-normal">{infill}</span>
+          <span className="font-semibold">Infill: </span><span className="font-normal">{job.infill}</span>
         </p>
       </div>
     </div>
