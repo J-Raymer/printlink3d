eb6432cd7c38dee8cac4135357861263f1605123
DH3773 <dylanhugheswork@gmail.com>
2024-02-15 15:11:47 -0800

    Renamed the button to 'HomeButton'

--- app/src/components/home_button.js
+++ app/src/components/home_button.js
@@ -1,4 +1,4 @@
-export default function Button({text}) {
+export default function HomeButton({text}) {
     return (
         <button className="bg-transparent hover:text-red-700 text-white font-bold py-2 px-4 rounded">{text}</button>
     );
