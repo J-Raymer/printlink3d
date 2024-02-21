4e132f26040d6af21e588c1fc10888e1ffee5bab
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-20 22:39:30 -0800

    Add steps section to home page

--- app/src/components/homePageStep.js
+++ app/src/components/homePageStep.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import "./homePageStep.css"
 
 function HomePageStep({step, title, text}) {
     return (
-        <div className="flex flex-col items-center mt-10">
-        <div className="flex items-center justify-center w-24 h-24 bg-transparent border-4 border-blue-200 rounded-full">
-          <span className="text-2xl font-bold">{step}</span>
+      <div className="flex flex-col items-center mt-10">
+        <div class="flex items-center justify-center w-24 h-24 step-circle rounded-full">
+          <span class="text-2xl font-bold">{step}</span>
         </div>
         <h2 className="mt-4 text-xl font-bold">{title}</h2>
         <div className="mt-2 text-center">
-          {text.map((element, index) => (
-            <React.Fragment key={index}>{element}</React.Fragment>
-          ))}
+           {text.map((element, index) => (
+             <React.Fragment key={index}>{element}</React.Fragment>
+           ))}
         </div>
       </div>
     )
