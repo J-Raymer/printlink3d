ad3939cec8395dc5af9b292dd539659714dd0586
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-23 00:15:01 -0800

    Add profile dropdown menu that appears on hover

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Outlet, useNavigate } from "react-router-dom";
-
+import { useState } from 'react';
 import './app.css';
 import Home from './routes/home.js';
 import Create from './routes/create.js';
@@ -7,6 +7,20 @@ import Browse from './routes/browse.js';
 
 function Layout() {
   const navigate = useNavigate();
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [delayHandler, setDelayHandler] = useState(null)
+
+    const handleMouseEnter = () => {
+        clearTimeout(delayHandler)
+        setDropdownVisible(true)
+    }
+
+    const handleMouseLeave = () => {
+        setDelayHandler(setTimeout(() => {
+          setDropdownVisible(false)
+      }, 500))
+    }
+
   return (
     <div>
       <nav className="flex justify-between w-full p-1 bg-transparent px-4 py-2 text-xl font-bold">
@@ -20,7 +34,27 @@ function Layout() {
           <button onClick={() => navigate('/browse')}>
             <h2>View Jobs</h2>
           </button>
-        </div>     
+          <div
+            className="relative"
+            onMouseEnter={() => handleMouseEnter()}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            <button>
+              <h2>Profile Name</h2>
+            </button>
+            {isDropdownVisible && (
+              <div
+                className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                onMouseEnter={() => handleMouseEnter()}
+                onMouseLeave={() => handleMouseLeave()}
+              >
+                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <button onClick={() => navigate('/profile')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Profile</button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </nav>
       <Outlet />
     </div>
@@ -30,10 +64,10 @@ function Layout() {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>} />
-        <Route path="create" element={<Create/>} />
-        <Route path="browse" element={<Browse/>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="create" element={<Create />} />
+        <Route path="browse" element={<Browse />} />
       </Route>
     </Routes>
   );
