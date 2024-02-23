ed6b44d5fcedc5a17ceddb5aa5d8e814b3fb2798
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-23 00:49:04 -0800

    Refactor profileDropdown.js

--- app/src/components/profileDropdown.js
+++ app/src/components/profileDropdown.js
@@ -5,21 +5,36 @@ import ordersIcon from '../images/orders_icon.png';
 
 function ProfileDropdown() {
   const navigate = useNavigate();
+  const items = [
+    {
+      icon: profileIcon,
+      text: 'Profile',
+      route: '/profile'
+    },
+    {
+      icon: ordersIcon,
+      text: 'Orders',
+      route: '/orders',
+    },
+    {
+      icon: settingsIcon,
+      text: 'Settings',
+      route: '/settings',
+    }
+  ]
+
+  const renderItems = () => {
+    return items.map((item, index) => (
+      <div key={index} className="p-3 flex items-center">
+        <img src={item.icon} className="w-10 h-10" alt={`${item.text} icon`} />
+        <button onClick={() => navigate(item.route)} className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100">{item.text}</button>
+      </div>
+    ));
+  }
 
   return (
     <>
-      <div className="p-3 flex items-center" role="menu">
-        <img src={profileIcon} className="w-10 h-10" alt="profile icon" />
-        <button onClick={() => navigate('/profile')} className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100" role="menuitem">Profile</button>
-      </div>
-      <div className="p-3 flex items-center" role="menu">
-        <img src={ordersIcon} className="w-10 h-10" alt="orders icon" />
-        <button onClick={() => navigate('/orders')} className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100" role="menuitem">Orders</button>
-      </div>
-      <div className="p-3 flex items-center" role="menu">
-        <img src={settingsIcon} className="w-10 h-10" alt="settings icon" />
-        <button onClick={() => navigate('/settings')} className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100" role="menuitem">Settings</button>
-      </div>
+      {renderItems()}
     </>
   )
 }
