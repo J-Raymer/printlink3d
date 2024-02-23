a65836081740684256f39c85cf2caca59fc90e7a
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-22 23:43:21 -0800

    Refactor nav bar

--- app/src/app.js
+++ app/src/app.js
@@ -9,16 +9,16 @@ function Layout() {
   const navigate = useNavigate();
   return (
     <div>
-      <nav className="flex justify-between w-full p-1">
-        <button className="bg-transparent rounded-lg px-4 py-2" onClick={() => navigate('/')}>
-          <h2 className="text-xl font-bold">PrintLink3D</h2>
+      <nav className="flex justify-between w-full p-1 bg-transparent px-4 py-2 text-xl font-bold">
+        <button onClick={() => navigate('/')}>
+          <h2>PrintLink3D</h2>
         </button>
         <div className="flex space-x-4">
-          <button className="bg-transparent rounded-lg px-4 py-2" onClick={() => navigate('/create')}>
-            <h2 className="text-xl font-bold">Order</h2>
+          <button onClick={() => navigate('/create')}>
+            <h2>Order</h2>
           </button>
-          <button className="bg-transparent rounded-lg px-4 py-2" onClick={() => navigate('/browse')}>
-            <h2 className="text-xl font-bold">View Jobs</h2>
+          <button onClick={() => navigate('/browse')}>
+            <h2>View Jobs</h2>
           </button>
         </div>     
       </nav>
