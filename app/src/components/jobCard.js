087c88206171ff7be5c7279f3d2be17fe8bab576
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-26 13:32:10 -0800

    Add Bid and Quantity to Job Card

--- app/src/components/jobCard.js
+++ app/src/components/jobCard.js
@@ -18,13 +18,20 @@ export default function JobCard({ job, isSelected, onSelectJob, onUnselectJob })
       <div className="md:flex-shrink-0">
         <img className="h-48 w-full object-cover md:w-48" src={boat} alt={job.fileName} />
       </div>
-      <div className="p-8">
+      <div className="pl-5 pt-5 text-gray-600">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{job.fileName}</div>
-        <p className="mt-2 text-gray-600">
-          <span className="font-semibold">Material: </span><span className="font-normal">{job.material}</span>
+        <p className="mt-2">
+          <span className="font-semibold mt-2">Material: </span><span className="font-normal">{job.material}</span>
         </p>
-        <p className="mt-2 text-gray-600">
+        <p className="mt-2">
           <span className="font-semibold">Infill: </span><span className="font-normal">{job.infill}</span>
+        </p>
+        {/* TODO dynamically add Bid and Quantity */}
+        <p className="mt-2">
+          <span className="font-semibold">Bid: </span><span className="font-normal">$10</span>
+        </p>
+        <p className="mt-2">
+          <span className="font-semibold">Quantity: </span><span className="font-normal">10</span>
         </p>
       </div>
     </div>
