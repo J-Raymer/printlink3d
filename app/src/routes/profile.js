08ebbcdf13af4525e811203e22ef6ec6f0384b1d
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-02-29 14:05:00 -0800

    Add ability to change profile picture

--- app/src/routes/profile.js
+++ app/src/routes/profile.js
@@ -1,21 +1,38 @@
 import default_pfp from '../images/default_pfp.jpg';
 import pencil_icon from '../images/pencil.png';
 import TextForm from '../components/textForm';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Profile() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  
+  const [pfp, setPfp] = useState(default_pfp);
+  const fileInputRef = useRef();
+
+  const handleFileSelection = (event) => {
+    if (event.target.files.length > 0) {
+      const fileReader = new FileReader();
+      fileReader.onloadend = () => {
+        setPfp(fileReader.result);
+      };
+      fileReader.readAsDataURL(event.target.files[0]);
+    }
+  };
+
+  const openFileExplorer = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <div className="flex mt-10 ml-10">
       <div className="w-32 h-32 overflow-hidden relative">
-        <img className="w-32 h-32 rounded-full object-cover" src={default_pfp} alt="Profile" />
-        <button className="absolute bottom-0 left-0 bg-gray-400 pl-2 pr-2 z-10 flex items-center rounded-full">
+        <img className="w-32 h-32 rounded-full object-cover" src={pfp} alt="Profile" />
+        <button onClick={openFileExplorer} className="absolute bottom-0 left-0 bg-gray-400 pl-2 pr-2 z-10 flex items-center rounded-full">
           <img src={pencil_icon} alt="Edit" className="mr-1 w-4 h-4" />
           Edit
         </button>
+        <input ref={fileInputRef} type="file" accept=".png, .jpg, .jpeg" style={{ display: 'none' }} onChange={handleFileSelection} />
       </div>
       <div className="w-1/2 ml-10">
         <h2 className="mb-2 mt-5">First Name</h2>
