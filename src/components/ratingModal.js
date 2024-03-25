5685d5938be2d9ca9cc72d746c5acf4ee75c30da
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-03-25 14:42:09 -0700

    Prevent submission if rating has not been given

--- src/components/ratingModal.js
+++ src/components/ratingModal.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
+import TextArea from './textArea';
 
 export default function RatingModal({ submitRating, isModalVisible }) {
   const [overallRating, setOverallRating] = useState(null);
   const [printQualityRating, setPrintQualityRating] = useState(null);
   const [communicationRating, setCommunicationRating] = useState(null);
   const [exchangeRating, setExchangeRating] = useState(null);
+  const [canSubmit, setCanSubmit] = useState(false);
 
   const overallRatings = ['Good', 'Neutral', 'Bad'];
   const numberRatings = [1, 2, 3, 4, 5];
+
+  React.useEffect(() => {
+    if (overallRating && printQualityRating && communicationRating && exchangeRating) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [overallRating, printQualityRating, communicationRating, exchangeRating]);
 
   return (
     isModalVisible && (
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex items-center justify-center">
         <div className="bg-white p-4 rounded-lg w-[600px] mx-auto">
-          <p className="font-semibold text-2xl text-center">Rate your experience with Jasper for Boat.stlffffffffffffffffffffffffffffffffffffffffffffff</p>
+          <p className="font-semibold text-2xl text-center">Rate your experience with Jasper for Boat.stl</p>
           <hr className="border w-4/5 mx-auto my-4" />
           <div className="flex items-center">
             <p className="text-2xl mr-4">Overall:</p>
@@ -70,15 +80,10 @@ export default function RatingModal({ submitRating, isModalVisible }) {
             </div>
           </div>
           <div className="mt-10">
-            <div className="text-2xl">Comment:</div>
-
-            <textarea
-              class="mt-5 px-2 py-1 bg-gray-50 border border-gray-300 text-gray-900 rounded-md focus:ring-blue-500 focus:border-blue-500 block"
-              rows="4"
-              cols="50"
-            />
+            <div className="text-2xl mb-5">Comment:</div>
+            <TextArea />
           </div>
-          <button className="float-right mt-4 bg-brand-blue text-white p-2 px-4 rounded" onClick={submitRating}>Submit</button>
+          <button className={`float-right mt-4 p-2 px-4 rounded text-white ${canSubmit ? 'bg-brand-blue' : 'bg-gray-300'}`} onClick={submitRating} disabled={!canSubmit}>Submit</button>
         </div>
       </div>
     )
